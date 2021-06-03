9a31d3ba02f1f9bca9dccc57266526efa3a3a102
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-03 14:46:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,10 @@ require 'pry'
 def load_library(file)
   hash_of_arrays = YAML.load(File.read(file))
   emoticons_hash = hash_of_arrays.each_with_object({}) do |(name, emoticons), final_hash|
-    emoticons.each_with_index do |emoticon, i|
-      final_hash[name] = 
+    final_hash[name] = {}
+    final_hash[name][:english] = emoticons[0]
+    final_hash[name][:japanese] = emoticons[1]
+  end
 end
 
 def get_japanese_emoticon
